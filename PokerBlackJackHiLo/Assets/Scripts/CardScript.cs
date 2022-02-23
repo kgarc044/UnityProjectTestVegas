bcdfeb32e7a47acc9e080d870510b27576af2208
kgarc044 <46533611+kgarc044@users.noreply.github.com>
2022-02-22 23:31:04 -0800

    Working black jack just need to fix some bugs

---
+++ PokerBlackJackHiLo/Assets/Scripts/CardScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+    public int value = 0;
+
+    public int GetValueOfCard()
+    {
+        return value;
+    }
+
+    public void SetValue(int newValue)
+    {
+        value = newValue;
+    }
+
+    public void SetSprite(Sprite newSprite)
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = newSprite;
+    }
+
+    public string GetSpriteName()
+    {
+        return GetComponent<SpriteRenderer>().sprite.name;
+    }
+
+    public void ResetCard()
+    {
+        Sprite back = GameObject.Find("Deck").GetComponent<DeckScript>().GetCardBack();
+        gameObject.GetComponent<SpriteRenderer>().sprite = back;
+        value = 0;
+    }
+}
---
+++ PokerBlackJackHiLo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Button dealButton;
+    public Button hitButton;
+    public Button standButton;
+    public Button betButton;
+
+    public Text standButtonText;
+    public Text scoreText;
+    public Text dealerScoreText;
+    public Text betsText;
+    public Text cashText;
+    public Text mainText;
+
+    private int standClicks = 0;
+
+    public GameObject hideCard;
+
+    int pot = 0;
+
+    public PlayerScript playerScript;
+    public PlayerScript dealerScript;
+
+
+    void Start()
+    {
+        dealButton.onClick.AddListener(() => DealClicked());
+        hitButton.onClick.AddListener(() => HitClicked());
+        standButton.onClick.AddListener(() => StandClicked());
+        betButton.onClick.AddListener(() => BetClicked());
+    }
+
+    private void StandClicked()
+    {
+        standClicks++;
+        if (standClicks > 1)
+        {
+            RoundOver();
+        }
+        HitDealer();
+        standButtonText.text = "Call";
+    }
+
+    private void HitClicked()
+    {
+        if (playerScript.cardIndex <= 10)
+        {
+            playerScript.GetCard();
+            scoreText.text = "Hand: " + playerScript.handValue.ToString();
+            if (playerScript.handValue > 20)
+            {
+                RoundOver();
+            }
+        }
+    }
+
+    private void DealClicked()
+    {
+        playerScript.ResetHand();
+        dealerScript.ResetHand();
+        mainText.gameObject.SetActive(false);
+        dealerScoreText.gameObject.SetActive(false);
+        GameObject.Find("Deck").GetComponent<DeckScript>().Shuffle();
+        playerScript.StartHand();
+        dealerScript.StartHand();
+        scoreText.text = "Hand: " + playerScript.handValue.ToString();
+        dealerScoreText.text = "Dealer Hand: " + dealerScript.handValue.ToString();
+        hideCard.GetComponent <Renderer>().enabled = true;
+        dealButton.gameObject.SetActive(false);
+        hitButton.gameObject.SetActive(true);
+        standButton.gameObject.SetActive(true);
+        standButtonText.text = "Stand";
+        pot = 40;
+        betsText.text = "Bets: $" + pot.ToString();
+        playerScript.AdjustMoney(-20);
+        cashText.text = "$" + playerScript.GetMoney().ToString();
+    }
+
+    private void HitDealer()
+    {
+        while (dealerScript.handValue < 16 && dealerScript.cardIndex < 10)
+        {
+            dealerScript.GetCard();
+            dealerScoreText.text = "Hand: " + dealerScript.handValue.ToString();
+            if (dealerScript.handValue > 20) RoundOver();
+        }
+    }
+    // Update is called once per frame
+    
+    void RoundOver()
+    {
+        bool playerBust = playerScript.handValue > 21;
+        bool dealerBust = dealerScript.handValue > 21;
+        bool player21 = playerScript.handValue == 21;
+        bool dealer21 = dealerScript.handValue == 21;
+        // If stand has been clicked less than twice, no 21s or busts, quit function
+        if (standClicks < 2 && !playerBust && !dealerBust && !player21 && !dealer21) return;
+        bool roundOver = true;
+        if (playerBust && dealerBust)
+        {
+            mainText.text = "All Bust: Bets returned";
+            playerScript.AdjustMoney(pot / 2);
+        }
+
+        else if (playerBust || (!dealerBust && dealerScript.handValue > playerScript.handValue))
+        {
+            mainText.text = "Dealer wins!";
+        }
+
+        else if (dealerBust || playerScript.handValue > dealerScript.handValue)
+        {
+            mainText.text = "You win!";
+            playerScript.AdjustMoney(pot);
+        }
+
+        else if (playerScript.handValue == dealerScript.handValue)
+        {
+            mainText.text = "Push: Bets returned";
+            playerScript.AdjustMoney(pot / 2);
+        }
+
+
+        else
+        {
+            roundOver = false;
+        }
+
+        if(roundOver)
+        {
+            hitButton.gameObject.SetActive(false);
+            standButton.gameObject.SetActive(false);
+            dealButton.gameObject.SetActive(true);
+            mainText.gameObject.SetActive(true);
+            dealerScoreText.gameObject.SetActive(true);
+            hideCard.GetComponent<Renderer>().enabled = false;
+            cashText.text = "$" + playerScript.GetMoney().ToString();
+            standClicks = 0;
+        }
+
+    }
+    void BetClicked()
+    {
+        Text newBet = betButton.GetComponentInChildren(typeof(Text)) as Text;
+        int intBet = int.Parse(newBet.text.ToString().Remove(0, 1));
+        playerScript.AdjustMoney(-intBet);
+        cashText.text = "$" + playerScript.GetMoney().ToString();
+        pot += (intBet * 2);
+        betsText.text = "Bets: $" + pot.ToString();
+    }
+}
