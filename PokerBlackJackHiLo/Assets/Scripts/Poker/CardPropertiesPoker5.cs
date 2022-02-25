cf87a6195f95780bca4682df22df92dfe787d87f
kgarc044 <46533611+kgarc044@users.noreply.github.com>
2022-02-24 17:42:04 -0800

    Small foundation of the poker game

---
+++ PokerBlackJackHiLo/Assets/Scripts/Poker/CardPropertiesPoker5.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardPropertiesPoker5 : MonoBehaviour
+{
+    public int value = 0;
+    public string suit = "";
+
+    public int GetValueOfCard()
+    {
+        return value;
+    }
+
+    public void SetValue(int newValue)
+    {
+        value = newValue;
+    }
+
+    public string GetSuitOfCard()
+    {
+        return suit;
+    }
+
+    public void SetSuit(string newSuit)
+    {
+        suit = newSuit;
+    }
+
+    public void SetSprite(Sprite newSprite)
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = newSprite;
+    }
+
+    public string GetSpriteName()
+    {
+        return GetComponent<SpriteRenderer>().sprite.name;
+    }
+
+    public void ResetCard()
+    {
+        Sprite back = GameObject.Find("Deck").GetComponent<DeckScript>().GetCardBack();
+        gameObject.GetComponent<SpriteRenderer>().sprite = back;
+        value = 0;
+    }
+}
---
+++ PokerBlackJackHiLo/Assets/Scripts/Poker/DeckPoker5.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckPoker5 : MonoBehaviour
+{
+    public Sprite[] cardSprites;
+    int[] cardValues = new int[52];
+    string[] cardSuit = new string[52];
+    int currentIndex = 0;
+
+    void GetCardValues()
+    {
+        int num = 0;
+        for (int i = 0; i < cardSprites.Length; i++)
+        {
+            num = i;
+            num %= 13;
+            if (num == 0)
+            {
+                num = 13;
+            }
+            cardValues[i] = num++;
+        }
+    }
+    public void Shuffle()
+    {
+        System.Random random = new System.Random();
+
+        for (int i = cardSprites.Length - 1; i > 0; --i)
+        {
+            int j = random.Next(i, cardSprites.Length);
+            Sprite face = cardSprites[i];
+            cardSprites[i] = cardSprites[j];
+            cardSprites[j] = face;
+
+            int value = cardValues[i];
+            cardValues[i] = cardValues[j];
+            cardValues[j] = value;
+        }
+
+        currentIndex = 1;
+
+    }
+    public void SetDeck(CardPropertiesPoker5 cardScript)
+    {
+        cardScript.SetSprite(cardSprites[currentIndex]);
+        cardScript.SetValue(cardValues[currentIndex]);
+        cardScript.SetSuit(cardSuit[currentIndex]);
+        currentIndex++;
+    }
+
+}
